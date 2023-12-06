e9af7963cfd02237f010e6bfe113afc71bf3f517
sabashatirishvili <shatirishvilis1@gmail.com>
2023-12-06 18:14:54 +0400

    Added links to Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,43 +1,52 @@
 import React from "react";
 import styles from "./footer.module.css";
 import Image from "next/image";
+import Link from "next/link";
 
 function Footer() {
+  const social = [
+    {
+      name: "facebook",
+      url: "https://www.facebook.com",
+      img:"/1.png"
+    },
+    {
+      name: "instagram",
+      url: "https://www.instagram.com",
+      img:"/2.png"
+    },
+    {
+      name: "x",
+      url: "https://www.x.com",
+      img:"/3.png"
+    },
+    {
+      name: "youtube",
+      url: "https://www.youtube.com",
+      img:"/4.png"
+    },
+  ]
   return (
     <div className={styles.container}>
 
       <span>@all rights reserved</span>
 
       <div className={styles.socialNetworks}>
-
-        <Image
-          src="/1.png"
-          width={15}
-          className={styles.icon}
-          height={15}
-          alt="facebook"
-        />
-        <Image
-          src="/2.png"
-          width={15}
-          className={styles.icon}
-          height={15}
-          alt="instagram"
-        />
-        <Image
-          src="/3.png"
-          width={15}
-          className={styles.icon}
-          height={15}
-          alt="x"
-        />
-        <Image
-          src="/4.png"
-          width={15}
-          className={styles.icon}
-          height={15}
-          alt="youtube"
-        />
+        {
+          social.map(icon => {
+            return (
+              <Link href={icon.url} key={icon.name}>
+                <Image
+                  src={icon.img}
+                  alt={icon.anme}
+                  width={15}
+                  height={15}
+                  className={styles.icon}
+                />
+              </Link>
+            )
+          })
+        }
       </div>
     </div>
   );
