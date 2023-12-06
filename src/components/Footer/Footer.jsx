33f6b363d31204ac5c00d92fd844a8c714e747be
sabashatirishvili <shatirishvilis1@gmail.com>
2023-12-06 18:09:35 +0400

    Revert "Added links to Footer"
    This reverts commit 50da09c15f4b7e9451178565fdaf353caa25c519.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,50 +1,43 @@
 import React from "react";
 import styles from "./footer.module.css";
 import Image from "next/image";
-import Link from "next/link";
 
 function Footer() {
-  const social = [
-    {
-      name: "facebook",
-      url: "https://www.facebook.com/",
-      img: "/1.png"
-    },
-    {
-      name: "instagram",
-      url: "https://www.instagram.com/",
-      img: "/2.png"
-    },
-    {
-      name: "x",
-      url: "https://www.x.com/",
-      img: "/3.png"
-    },
-    {
-      name: "youtube",
-      url: "https://www.youtube.com/",
-      img: "/4.png"
-    },
-  ]
   return (
     <div className={styles.container}>
 
       <span>@all rights reserved</span>
 
       <div className={styles.socialNetworks}>
-        {social.map(icon => {
-          return (
-            <Link href={icon.url} target="_blank">
-              <Image
-                src={icon.img}
-                width={15}
-                height={15}
-                className={icon.name}
-                alt={icon.name}
-              />
-            </Link>
-          )
-        })}
+
+        <Image
+          src="/1.png"
+          width={15}
+          className={styles.icon}
+          height={15}
+          alt="facebook"
+        />
+        <Image
+          src="/2.png"
+          width={15}
+          className={styles.icon}
+          height={15}
+          alt="instagram"
+        />
+        <Image
+          src="/3.png"
+          width={15}
+          className={styles.icon}
+          height={15}
+          alt="x"
+        />
+        <Image
+          src="/4.png"
+          width={15}
+          className={styles.icon}
+          height={15}
+          alt="youtube"
+        />
       </div>
     </div>
   );
