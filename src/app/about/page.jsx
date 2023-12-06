6531b91f530e219e20fd17a5b7f686c74a7deada
sabashatirishvili <shatirishvilis1@gmail.com>
2023-12-06 17:36:20 +0400

    Added About and Contact

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,25 +1,66 @@
-import React from 'react'
-import Image from 'next/image'
-import styles from './page.module.css'
+import React from "react";
+import styles from "./page.module.css";
+import Image from "next/image";
+import Button from "@/components/button/Button";
 
 function About() {
   return (
     <main className={styles.container}>
       <div className={styles.imgContainer}>
-        <Image 
-        src="https://images.pexels.com/photos/3194521/pexels-photo-3194521.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-        alt='group photo'
-        className={styles.img}
-        fill="true"
-        ></Image>
+        <Image
+          src={
+            "https://images.pexels.com/photos/3194521/pexels-photo-3194521.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          }
+          fill={true}
+          alt="group photo"
+          className={styles.img}
+        />
         <div className={styles.imgText}>
-          <h1 className={styles.imgTitle}>Digital Storyteller</h1>
-          <h2 className={styles.imgDescription}>Handcrafting award winning digital experiences</h2>
+          <h1 className={styles.imgTitle}>Digital Storytellers</h1>
+          <h2 className={styles.imgDesc}>
+            Handcrafting award winning digital experiences
+          </h2>
         </div>
       </div>
-      
+      <section className={styles.textContainer}>
+        <div className={styles.item}>
+          <h1>Who Are We</h1>
+          <p className={styles.desc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
+            quae dolor, optio voluptatibus magnam iure esse tempora beatae. A
+            suscipit eos. Animi quibusdam cum omnis officiis voluptatum quo ea
+            eveniet? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Ducimus quae dolor, optio voluptatibus magnam iure esse tempora
+            beatae, a suscipit eos. Animi quibusdam cum omnis officiis
+            <br />
+            <br />
+            voluptatum quo ea eveniet? Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Ducimus quae dolor, optio voluptatibus magnam iure
+            esse tempora beatae, a suscipit eos. Animi quibusdam cum omnis
+            officiis voluptatum quo ea eveniet?
+          </p>
+        </div>
+        <div className={styles.item}>
+          <h1>What We Do?</h1>
+          <p className={styles.desc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
+            quae dolor, optio voluptatibus magnam iure esse tempora beatae, a
+            suscipit eos. Animi quibusdam cum omnis officiis voluptatum quo ea
+            eveniet? Lorem ipsum dolor sit amet consectetur adipisicing elit. -
+            Creative Illustrations
+            <br /> <br />
+            - Dynamic Websites
+            <br />
+            <br />
+            - Fast and Handy
+            <br />
+            <br />- Mobile Apps
+          </p>
+          <Button title={"Contact"} url={"/contact"} />
+        </div>
+      </section>
     </main>
-  )
+  );
 }
 
-export default About
+export default About;
