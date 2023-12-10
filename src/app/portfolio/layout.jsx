109acad6e9d9d893871e8c21b6c20e696c9415f1
sabashatirishvili <shatirishvilis1@gmail.com>
2023-12-10 22:16:06 +0400

    Added Blog and Portfolio

---
+++ src/app/portfolio/layout.jsx
@@ -0,0 +1,15 @@
+import React, { Children } from 'react'
+import styles from './page.module.css'
+
+function Layout({ children }) {
+  return (
+    <div>
+      <h1 className={styles.mainTitle}>Our Works</h1>
+      {children}
+    </div>
+    
+  )
+}
+
+export default Layout;
+
--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import styles from "./page.module.css";
+import Link from "next/link";
 
 function Portfolio() {
   return (
-    <div>portfolio Page</div>
-  )
+    <div className={styles.container}>
+      <h1 className={styles.sectTitle}>Choose a Gallery</h1>
+      <div className={styles.items}>
+        <Link href="/portfolio/illustrations" className={styles.item}>
+          <span className={styles.title}>Illustrations</span>
+        </Link>
+        <Link href="/portfolio/websites" className={styles.item}>
+          <span className={styles.title}>Websites</span>
+        </Link>
+        <Link href="/portfolio/applications" className={styles.item}>
+          <span className={styles.title}>Applications</span>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
-export default Portfolio
+export default Portfolio;
