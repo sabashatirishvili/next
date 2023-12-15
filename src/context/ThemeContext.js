56609cd6a235a1bf86cd0e73e5177e44d8c6c6ae
sabashatirishvili <shatirishvilis1@gmail.com>
2023-12-15 22:50:33 +0400

    Added Dark mode interactivity

---
+++ src/context/ThemeContext.js
@@ -0,0 +1,23 @@
+"use client";
+
+import { createContext, useState } from "react";
+
+export const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [mode, setMode] = useState("light");
+
+  const toggle = () => {
+    if (mode === "dark") {
+      setMode("light");
+    } else {
+      setMode("dark");
+    }
+  };
+
+  return (
+    <ThemeContext.Provider value={{ mode, toggle }}>
+      <div className={`theme ${mode}`}>{children}</div>
+    </ThemeContext.Provider>
+  );
+};
